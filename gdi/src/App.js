665734bb07ad1674f90e8c48fc92365f83a5a6d2
eghsu <129780469+eghsu@users.noreply.github.com>
2023-09-22 11:33:32 -0500

    Update App.js

--- gdi/src/App.js
+++ gdi/src/App.js
@@ -7,5 +7,19 @@ import './style.css';
 	// import Header and Homepage
 	// include them inside of App
 	// export the App component!
+import React from 'react'; 
+import './style.css'; 
+import Header from './components/Header.jsx'; 
+import Homepage from './pages/HomePage/jsx'; 
 
+function App() {
+	return(
+		<div>
+			<Header />
+			<Homepage />
+		</div>
+	) 
+}
+
+export default App; 
 
